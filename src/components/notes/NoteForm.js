708c0a9147becf9b31d66e949de8818fae2e7568
Ryan Mehta <ryan.mehta@gmail.com>
2019-03-18 13:37:44 -0700

    add error handling and loading stuff

--- src/components/notes/NoteForm.js
+++ src/components/notes/NoteForm.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FaAsterisk } from 'react-icons/fa';
 import styles from './NoteForm.css';
 
-function NotesForm({ title, body, createNote, titleChange, bodyChange }) {
+function NotesForm({ title, body, error, loading, createNote, titleChange, bodyChange }) {
   return (
     <form className={styles.NotesForm} onSubmit={createNote.bind(null, title, body)}>
+      {error && <section className={styles.error}>
+        <p>Error: {error}</p>
+      </section>}
       <input
         type="text"
         name="title"
@@ -16,12 +20,16 @@ function NotesForm({ title, body, createNote, titleChange, bodyChange }) {
         name="body" value={body}
         onChange={bodyChange}
       ></textarea>
-      <button>Create</button>
+      <button disabled={loading}>
+        <span className={loading ? styles.spinner : ''}>{loading ? <FaAsterisk /> : 'Create'}</span>
+      </button>
     </form>
   );
 }
 
 NotesForm.propTypes = {
+  error: PropTypes.string,
+  loading: PropTypes.bool.isRequired,
   title: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
   createNote: PropTypes.func.isRequired,
