d5008a8ec8256e03b55b8fd033e10e16e19207f5
Ryan Mehta <ryan.mehta@gmail.com>
2019-03-18 10:44:40 -0700

    header

---
+++ src/components/header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Header.css';
+
+export function Header() {
+  return (
+    <header className={styles.Header}>
+      <h1>Notes</h1>
+    </header>
+  );
+}
